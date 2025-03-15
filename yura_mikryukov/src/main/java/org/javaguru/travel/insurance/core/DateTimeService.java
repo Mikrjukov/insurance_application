eba703a710c3bd9521328a56ab230c5db86b8d51
yura <yuramikrjukov@gmail.com>
2025-03-15 11:59:10 +0200

    DateTime fix

--- yura_mikryukov/src/main/java/org/javaguru/travel/insurance/core/DateTimeService.java
+++ yura_mikryukov/src/main/java/org/javaguru/travel/insurance/core/DateTimeService.java
@@ -1,9 +1,12 @@
 package org.javaguru.travel.insurance.core;
 
 
+import org.springframework.stereotype.Component;
+
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
+@Component
 public class DateTimeService {
 
     public long calculateDaysBetween(LocalDate dateFrom, LocalDate dateTo) {
